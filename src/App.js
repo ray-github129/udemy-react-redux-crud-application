244e6a752af224baa3fabbb354781e46be88460b
ray <ray129app@gmail.com>
2020-05-23 00:38:30 +0900

    Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 
-function App() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello, World!</h1>
+//     </div>
+//   );
+// }
+
+const App = () => {
+	return <div>Hi</div>
+	}
 
 export default App;
